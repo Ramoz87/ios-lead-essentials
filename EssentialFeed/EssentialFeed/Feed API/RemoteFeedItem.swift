aa2fb5f682f8ab0d442f17fd0dbaec85c53b2796
Yury Ramazanov <yury.ramazanov@gmail.com>
2024-12-24 13:13:41 +0100

    Add RemoteFeedItem DTO

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,19 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Yury Ramazanov on 24.12.2024.
+//
+
+import Foundation
+
+struct RemoteFeedItem: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+    
+    var item: FeedItem {
+        FeedItem(id: id, description: description, location: location, imageURL: image)
+    }
+}
