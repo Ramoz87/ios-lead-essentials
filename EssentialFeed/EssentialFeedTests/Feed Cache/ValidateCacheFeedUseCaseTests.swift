1f55737e7badd16d7133e909043f708fcddb5a59
Yury Ramazanov <yury.ramazanov@gmail.com>
2024-12-26 13:20:16 +0100

    New ValidateCacheFeedUseCase. LoadFeedLoader doesn't perform any command on creation before validate feed

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateCacheFeedUseCaseTests.swift
@@ -0,0 +1,27 @@
+//
+//  ValidateCacheFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Yury Ramazanov on 26.12.2024.
+//
+
+import XCTest
+import EssentialFeed
+
+final class ValidateCacheFeedUseCaseTests: XCTestCase {
+
+    func test_init_doesNotPerformAnyCommands() {
+        let (_, store) = makeSUT()
+        XCTAssertEqual(store.commands, [])
+    }
+    
+    //MARK: - Helpers
+        
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, date: currentDate)
+        trackMemoryLeaks(store, file: file, line: line)
+        trackMemoryLeaks(store, file: file, line: line)
+        return (sut, store)
+    }
+}
