d9e5af28274bff87dcda1a8bc0cdbc2625207cb9
Yury Ramazanov <yury.ramazanov@gmail.com>
2024-12-30 13:08:10 +0100

    Extract delete to helper method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -176,13 +176,9 @@ final class CodableFeedStoreTests: XCTestCase {
     
     func test_delete_emptyCache_cacheIsEmpty() {
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache deletion")
         
-        sut.deleteCachedFeed { deletionError in
-            XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        let deletionError = delete(from: sut)
+        XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
         
         expect(sut, retrieve: .empty)
     }
@@ -193,12 +189,8 @@ final class CodableFeedStoreTests: XCTestCase {
         let timestamp = Date()
         
         insert(feed: feed, timestamp: timestamp, to: sut)
-        let exp = expectation(description: "Wait for cache deletion")
-        sut.deleteCachedFeed { deletionError in
-            XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        let deletionError = delete(from: sut)
+        XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
         
         expect(sut, retrieve: .empty)
     }
@@ -262,6 +254,17 @@ final class CodableFeedStoreTests: XCTestCase {
         var error: Error?
         sut.insert(feed, timestamp: timestamp) { insertError in
             error = insertError
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+        return error
+    }
+    
+    private func delete(from sut: CodableFeedStore) -> Error? {
+        let exp = expectation(description: "Wait for cache delete")
+        var error: Error?
+        sut.deleteCachedFeed { deleteError in
+            error = deleteError
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
