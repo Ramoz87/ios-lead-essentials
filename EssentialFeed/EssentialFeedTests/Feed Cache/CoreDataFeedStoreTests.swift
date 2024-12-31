bb3d5b72889e5bfa7c60ec37ff9fb1fdf4b69001
Yury Ramazanov <yury.ramazanov@gmail.com>
2024-12-31 12:00:01 +0100

    Add CoreDataFeedStoretests

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,84 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeed
+//
+//  Created by Yury Ramazanov on 31.12.2024.
+//
+
+import XCTest
+
+final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
+    func test_retrieve_retrieveError_deliverError() {
+        
+    }
+    
+    func test_retrieve_retrieveError_noSideEffect() {
+        
+    }
+    
+    func test_insert_insertError_deliverError() {
+        
+    }
+    
+    func test_insert_insertError_noSideEffect() {
+        
+    }
+    
+    func test_delete_deleteError_deliverError() {
+        
+    }
+    
+    func test_delete_deleteError_noSideEffect() {
+        
+    }
+    
+    func test_retrieve_emptyCache_deliverEmptyResult() {
+        
+    }
+    
+    func test_retrieve_emptyCache_noSideEffect() {
+        
+    }
+    
+    func test_retrieve_noEmptyCache_deliverCache() {
+        
+    }
+    
+    func test_retrieve_noEmptyCache_noSideEffect() {
+        
+    }
+    
+    func test_insert_emptyCache_deliverNoError() {
+        
+    }
+    
+    func test_insert_noEmptyCache_deliverNoError() {
+        
+    }
+    
+    func test_insert_noEmptyCache_overrideCache() {
+        
+    }
+    
+    func test_delete_emptyCache_deliverNoError() {
+        
+    }
+    
+    func test_delete_emptyCache_noSideEffect() {
+        
+    }
+    
+    func test_delete_noEmptyCache_deliverNoError() {
+        
+    }
+    
+    func test_delete_noEmptyCache_deleteCache() {
+        
+    }
+    
+    func test_sideEffectOperationsRunSerially() {
+        
+    }
+    
+    
+}
