3ec353ad7676744ed75b28051005564cab94dc20
Yury Ramazanov <yury.ramazanov@gmail.com>
2024-12-31 14:53:29 +0100

    Add EssentialFeedCacheIntegrationTests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Yury Ramazanov on 31.12.2024.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+}
