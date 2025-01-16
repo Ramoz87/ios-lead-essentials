92b0e3f0259c983d89f5ae0f69ae3ddcd8dce46e
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-01-16 14:16:11 +0100

    Load feed when view loaded

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -14,7 +14,7 @@ private class FeedViewController: UIViewController {
     
     convenience init(loader: FeedLoader) {
         self.init()
-        self.feedLoader = feedLoader
+        self.feedLoader = loader
     }
     
     override func viewDidLoad() {
@@ -25,10 +25,27 @@ private class FeedViewController: UIViewController {
 final class FeedViewControllerTests: XCTestCase {
 
     func test_init_doesNotLoadFeed() {
-        let loader = LoaderSpy()
-        let sut = FeedViewController(loader: loader)
+        let (_, loader) = makeSUT()
         
         XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    func test_viewDidLoad_loadsFeed() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (FeedViewController, LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        trackMemoryLeaks(sut, file: file, line: line)
+        trackMemoryLeaks(loader, file: file, line: line)
+        return (sut, loader)
     }
     
     class LoaderSpy: FeedLoader {
