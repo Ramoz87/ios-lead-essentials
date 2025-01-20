e204b4d36b238779eb94341e465961034dc133a0
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-01-20 15:52:29 +0100

    Render loaded image on Image view

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -71,7 +71,9 @@ public class FeedViewController: UITableViewController {
         cell.descriptionLabel.text = cellModel.description
         
         cell.feedImageContainer.startShimmering()
-        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] _ in
+        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+            let data = try? result.get()
+            cell?.feedImageView.image = data.map(UIImage.init) ?? nil
             cell?.feedImageContainer.stopShimmering()
         }
         
