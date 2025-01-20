313fbbf0e2385f8eb5dc8e8d8be3739af340c565
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-01-20 15:56:44 +0100

    Show retry button when image load failed

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -70,10 +70,12 @@ public class FeedViewController: UITableViewController {
         cell.locationLabel.text = cellModel.location
         cell.descriptionLabel.text = cellModel.description
         
+        cell.feedImageRetryButton.isHidden = true
         cell.feedImageContainer.startShimmering()
         tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
             let data = try? result.get()
             cell?.feedImageView.image = data.map(UIImage.init) ?? nil
+            cell?.feedImageRetryButton.isHidden = data != nil
             cell?.feedImageContainer.stopShimmering()
         }
         
