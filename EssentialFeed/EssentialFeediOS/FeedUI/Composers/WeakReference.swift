e7ae6da2eebab90f31a3500bd30acfdce3774a44
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-01-28 13:11:59 +0100

    Extrct helper composition classes to seperate files

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/WeakReference.swift
@@ -0,0 +1,28 @@
+//
+//  WeakReference.swift
+//  EssentialFeed
+//
+//  Created by Yury Ramazanov on 28.01.2025.
+//
+
+import UIKit
+
+struct WeakReference<T: AnyObject> {
+    private weak var object: T?
+    
+    init(object: T) {
+        self.object = object
+    }
+}
+
+extension WeakReference: FeedLoadingView where T: FeedLoadingView {
+    func display(_ model: FeedLoadingViewModel) {
+        object?.display(model)
+    }
+}
+
+extension WeakReference: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
