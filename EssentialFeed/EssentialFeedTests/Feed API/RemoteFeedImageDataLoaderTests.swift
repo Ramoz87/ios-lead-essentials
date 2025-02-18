858d22532da6a428083e82cdce63eeb284147693
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-02-18 14:03:48 +0100

    RemoveFeedImageDataLoader doesn't perform any request on init

---
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -0,0 +1,34 @@
+//
+//  RemoteFeedImageDataLoaderTests.swift
+//  EssentialFeed
+//
+//  Created by Yury Ramazanov on 18.02.2025.
+//
+
+import XCTest
+
+class RemoteFeedImageDataLoader {
+    init(client: Any) {
+    }
+}
+
+class RemoteFeedImageDataLoaderTests: XCTestCase {
+
+    func test_init_doesNotPerformAnyURLRequest() {
+        let (_, client) = makeSUT()
+
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+
+    private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackMemoryLeaks(sut, file: file, line: line)
+        trackMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
+
+    private class HTTPClientSpy {
+        var requestedURLs = [URL]()
+    }
+}
