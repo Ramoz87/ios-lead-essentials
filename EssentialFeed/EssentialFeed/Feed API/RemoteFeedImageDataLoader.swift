eecc10c42fd7af64ad956c70870ac80eec11b6fd
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-02-18 15:48:06 +0100

    Extract RemoteFeedImageDataLoader to production

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -0,0 +1,57 @@
+//
+//  RemoteFeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Yury Ramazanov on 18.02.2025.
+//
+import Foundation
+
+public final class RemoteFeedImageDataLoader {
+    private let client: HTTPClient
+    
+    private final class HTTPTaskWrapper: FeedImageDataLoaderTask {
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
+        
+        var wrapped: HTTPClientTask?
+        
+        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+        
+        func cancel() {
+            completion = nil
+            wrapped?.cancel()
+        }
+    }
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    @discardableResult
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = HTTPTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            switch result {
+            case let .success((data, response)):
+                if response.statusCode == 200, !data.isEmpty {
+                    task.complete(with:.success(data))
+                } else {
+                    task.complete(with:.failure(Error.invalidData))
+                }
+            case let .failure(error): task.complete(with:.failure(error))
+            }
+        }
+        return task
+    }
+}
