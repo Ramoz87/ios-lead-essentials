03ac67423d17c5bb67a461232898a4cde855f64e
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-02-19 13:50:30 +0100

    Remove all stores in coordinator when CoreDataStore deinit

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -12,6 +12,10 @@ final public class CoreDataFeedStore {
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
+    deinit {
+        cleanUpReferencesToPersistentStores()
+    }
+    
     public init(storeUrl: URL) throws {
         let bundle = Bundle(for: CoreDataFeedStore.self)
         container = try NSPersistentContainer.load(modelName: "FeedCache", url: storeUrl, in: bundle)
@@ -21,5 +25,12 @@ final public class CoreDataFeedStore {
     func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
         let context = self.context
         context.perform { action(context) }
+    }
+    
+    private func cleanUpReferencesToPersistentStores() {
+        context.performAndWait {
+            let coordinator = self.container.persistentStoreCoordinator
+            try? coordinator.persistentStores.forEach(coordinator.remove)
+        }
     }
 }
