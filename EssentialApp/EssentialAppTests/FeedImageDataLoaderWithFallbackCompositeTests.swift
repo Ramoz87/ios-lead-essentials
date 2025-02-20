6a5508ebadfb9a4cac30c618886c12ccaca99a8f
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-02-20 13:07:09 +0100

    FeedImageDataLoaderWithFallbackComposite does not load data on init

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,59 @@
+//
+//  FeedImageDataLoaderWithFallbackCompositeTests.swift
+//  EssentialApp
+//
+//  Created by Yury Ramazanov on 20.02.2025.
+//
+
+import XCTest
+import EssentialFeed
+
+final class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    
+    private let primary: FeedImageDataLoader
+    private let fallback: FeedImageDataLoader
+    
+    private class Task: FeedImageDataLoaderTask {
+        func cancel() {
+            
+        }
+    }
+    
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> any FeedImageDataLoaderTask {
+        return Task()
+    }
+}
+
+final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+    
+    func test_init_doesNotLoadImageData() {
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        _ = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+    
+    private class LoaderSpy: FeedImageDataLoader {
+        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        
+        private struct Task: FeedImageDataLoaderTask {
+            func cancel() {}
+        }
+        
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> any FeedImageDataLoaderTask {
+            messages.append((url, completion))
+            return Task()
+        }
+    }
+}
