d5ed77acec6c94452187b9f1c77170d107a6da14
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-02-24 14:58:31 +0100

    Improve UI tests with predefined response with eliminations of network calls

--- EssentialApp/EssentialApp/DebugSceneDelegate.swift
+++ EssentialApp/EssentialApp/DebugSceneDelegate.swift
@@ -19,22 +19,71 @@ final class DebugSceneDelegate: SceneDelegate {
     }
     
     override func makeRemoteClient() -> HTTPClient {
-        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
-            return AlwaysFailingHTTPClient()
+        if let connectivity = UserDefaults.standard.string(forKey: "connectivity") {
+            return DebuggingHTTPClient(connectivity: connectivity)
         }
         
         return super.makeRemoteClient()
     }
 }
 
-private class AlwaysFailingHTTPClient: HTTPClient {
+private class DebuggingHTTPClient: HTTPClient {
     private class Task: HTTPClientTask {
         func cancel() {}
     }
     
+    private let connectivity: String
+    
+    init(connectivity: String) {
+        self.connectivity = connectivity
+    }
+    
     func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-        completion(.failure(NSError(domain: "offline", code: 0)))
+        switch connectivity {
+        case "online": completion(successResult(for: url))
+        default: completion(failureResult())
+        }
         return Task()
+    }
+    
+    private func failureResult() -> HTTPClient.Result {
+        .failure(NSError(domain: "offline", code: 0))
+    }
+    
+    private func successResult(for url: URL) -> HTTPClient.Result {
+        .success(makeSuccessfulResponse(for: url))
+    }
+    
+    private func makeSuccessfulResponse(for url: URL) -> (Data, HTTPURLResponse) {
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        return (data(for: url), response)
+    }
+    
+    private let imageUrl = "http://image.com"
+    
+    private func data(for url: URL) -> Data {
+        switch url.absoluteString {
+        case imageUrl: makeImageData()
+        default: makeFeedData()
+        }
+    }
+    
+    private func makeFeedData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": [
+            ["id": UUID().uuidString, "image": imageUrl],
+            ["id": UUID().uuidString, "image": imageUrl]
+        ]])
+    }
+    
+    private func makeImageData() -> Data {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(UIColor.red.cgColor)
+        context.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!.pngData()!
     }
 }
 #endif
