b3d03e26d50d17497bf3826d59d3010ab054c38d
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-03-06 09:39:41 +0100

    Replace LoadImageCommentsFromRemoteUseCaseTests with ImageCommentsMapperTests

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderDataMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderDataMapperTests.swift
@@ -8,9 +8,9 @@
 import XCTest
 import EssentialFeed
 
-final class FeedItemsMapperTests: XCTestCase {
+final class RemoteFeedLoaderDataMapperTests: XCTestCase {
     
-    func test_load_withNon200HTTPCode_shouldThrowsError() throws {
+    func test_map_throwsErrorOnNon200HTTPCode() throws {
         let json = makeJSON([])
         let codes = [199, 300, 400, 500]
     
@@ -21,20 +21,20 @@ final class FeedItemsMapperTests: XCTestCase {
         }
     }
     
-    func test_load_with200HTTPCodeAndInvalidJSON_shouldThorwsError() {
+    func test_map_throwsErrorOn200HTTPCodeAndInvalidJSON() {
         let json = Data("invalid json".utf8)
         XCTAssertThrowsError (
             try RemoteFeedLoaderDataMapper.map(json, HTTPURLResponse(statusCode: 200))
         )
     }
     
-    func test_load_with200HTTPCodeAndEmptyJSON_shouldDeliverEmptyResult() throws {
+    func test_map_deliverEmptyResultOn200HTTPCodeAndEmptyJSON() throws {
         let json = makeJSON([])
         let result = try RemoteFeedLoaderDataMapper.map(json, HTTPURLResponse(statusCode: 200))
         XCTAssertEqual(result, [])
     }
     
-    func test_load_with200HTTPCodeAndNotEmptyJSON_shouldDeliverResult() throws {
+    func test_map_deliverResultOn200HTTPCodeAndNotEmptyJSON() throws {
         let item1 = makeItem(id: UUID(),
                              url: URL(string: "http://a-url.com")!)
         
@@ -61,16 +61,5 @@ final class FeedItemsMapperTests: XCTestCase {
         ].compactMapValues { $0 }
         
         return (item, json)
-    }
-    
-    private func makeJSON(_ items: [[String: Any]]) -> Data {
-        let json = ["items": items]
-        return try! JSONSerialization.data(withJSONObject: json)
-    }
-}
-
-private extension HTTPURLResponse {
-    convenience init(statusCode: Int) {
-        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
     }
 }
