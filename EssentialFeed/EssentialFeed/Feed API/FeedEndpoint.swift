c0819c3c5754245581eed55b244e5e9a105b9f31
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-04-04 13:15:55 +0200

    Add limit and after_id params to get method

--- EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -7,12 +7,20 @@
 import Foundation
 
 public enum FeedEndpoint {
-    case get
-
+    case get(limit: Int = 10, after: FeedImage? = nil)
+    
     public func url(baseURL: URL) -> URL {
         switch self {
-        case .get:
-            return baseURL.appendingPathComponent("/v1/feed")
+        case .get(let limit, let image):
+            var components = URLComponents()
+            components.scheme = baseURL.scheme
+            components.host = baseURL.host
+            components.path = baseURL.path + "/v1/feed"
+            components.queryItems = [
+                URLQueryItem(name: "limit", value: String(limit)),
+                image.map { URLQueryItem(name: "after_id", value: $0.id.uuidString) },
+            ].compactMap { $0 }
+            return components.url!
         }
     }
 }
