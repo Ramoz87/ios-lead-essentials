8844b23b4e8b66c24438bf84c28e30a32d78b13b
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-04-10 11:13:59 +0200

    Synchronous FeedImageDataLoader

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -31,34 +31,19 @@ extension LocalFeedImageDataLoader: FeedImageDataCache {
 
 extension LocalFeedImageDataLoader: FeedImageDataLoader {
     
-    private final class LoadImageDataTask: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-                
-        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            completion = nil
-        }
-    }
-    
     public enum LoadError: Swift.Error {
         case failed
         case notFound
     }
     
-    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = LoadImageDataTask(completion)
-        let result = Swift.Result { try store.retrieve(dataForURL: url) }
-        task.complete( with: result
-            .mapError { _ in LoadError.failed }
-            .flatMap { data in data.map { .success($0) } ?? .failure(LoadError.notFound) })
-        
-        return task
+    public func loadImageData(from url: URL) throws -> Data {
+        do {
+            if let data = try store.retrieve(dataForURL: url) {
+                return data
+            }
+        } catch {
+            throw LoadError.failed
+        }
+        throw LoadError.notFound
     }
 }
