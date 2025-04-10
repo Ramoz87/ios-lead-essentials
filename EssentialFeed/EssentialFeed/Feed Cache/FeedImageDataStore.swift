32fc28556502314b77440c11867dc8ba55b8a207
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-04-10 10:34:05 +0200

    Synchronous methods signature and depricarted async methods in FeedImageDataStore

--- EssentialFeed/EssentialFeed/Feed Cache/FeedImageDataStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedImageDataStore.swift
@@ -11,6 +11,44 @@ public protocol FeedImageDataStore {
     typealias RetrieveResult = Result<Data?, Error>
     typealias InsertResult = Result<Void, Error>
 
+    func retrieve(dataForURL url: URL) throws -> Data?
+    func insert(_ data: Data, for url: URL) throws
+    
+    @available(*, deprecated)
     func retrieve(dataForURL url: URL, completion: @escaping (RetrieveResult) -> Void)
+    @available(*, deprecated)
     func insert(_ data: Data, for url: URL, completion: @escaping (InsertResult) -> Void)
+}
+
+public extension FeedImageDataStore {
+    func insert(_ data: Data, for url: URL) throws {
+        let group = DispatchGroup()
+        
+        group.enter()
+        var result: InsertResult!
+        insert(data, for: url) {
+            result = $0
+            group.leave()
+        }
+        group.wait()
+        
+        return try result.get()
+    }
+    
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        let group = DispatchGroup()
+        
+        group.enter()
+        var result: RetrieveResult!
+        retrieve(dataForURL: url) {
+            result = $0
+            group.leave()
+        }
+        group.wait()
+        
+        return try result.get()
+    }
+    
+    func retrieve(dataForURL url: URL, completion: @escaping (RetrieveResult) -> Void) {}
+    func insert(_ data: Data, for url: URL, completion: @escaping (InsertResult) -> Void) {}
 }
