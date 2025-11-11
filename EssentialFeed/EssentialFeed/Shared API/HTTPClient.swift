6657c38705c7872250daf51f52ebf703ce73364b
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-11-11 16:11:30 +0100

    New HTTPClient get async method

--- EssentialFeed/EssentialFeed/Shared API/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/Shared API/HTTPClient.swift
@@ -16,5 +16,22 @@ public protocol HTTPClient {
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
     @discardableResult
+    @available(*, deprecated, message: "Use async alternative")
     func get(from url: URL, completion: @Sendable @escaping (Result) -> Void) -> HTTPClientTask
+    
+    func get(from url: URL) async throws -> (Data, HTTPURLResponse)
+}
+
+public extension HTTPClient {
+    func get(from url: URL) async throws -> (Data, HTTPURLResponse) {
+        nonisolated(unsafe) var task: HTTPClientTask?
+        
+        return try await withTaskCancellationHandler {
+            try await withCheckedThrowingContinuation { continuation in
+                task = get(from: url) { continuation.resume(with: $0) }
+            }
+        } onCancel: {
+            task?.cancel()
+        }
+    }
 }
