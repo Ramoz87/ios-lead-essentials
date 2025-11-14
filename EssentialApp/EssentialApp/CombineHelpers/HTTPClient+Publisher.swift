2adef5c4580c9f781c46387cb5200594a0682cf5
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-11-14 12:37:04 +0100

    Migrate EssentialApp to new HTTPClient async API while keeping Combine API

--- EssentialApp/EssentialApp/CombineHelpers/HTTPClient+Publisher.swift
+++ EssentialApp/EssentialApp/CombineHelpers/HTTPClient+Publisher.swift
@@ -9,16 +9,24 @@ import Combine
 import Foundation
 import EssentialFeed
 
+@MainActor
 public extension HTTPClient {
     typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
-
+    
     func getPublisher(url: URL) -> Publisher {
-        var task: HTTPClientTask?
+        var task: Task<Void, Never>?
         
         return Deferred {
             Future { completion in
                 nonisolated(unsafe) let uncheckedCompletion = completion
-                task = get(from: url, completion: { uncheckedCompletion($0) })
+                task = Task.immediate {
+                    do {
+                        let result = try await self.get(from: url)
+                        uncheckedCompletion(.success(result))
+                    } catch {
+                        uncheckedCompletion(.failure(error))
+                    }
+                }
             }
         }
         .handleEvents(receiveCancel: { task?.cancel() })
