19eeeb323745c489d9a9ced09a588b850b01e66e
Yury Ramazanov <yury.ramazanov@gmail.com>
2025-11-26 11:37:10 +0100

    Introduce Generic LoaderSpy with async/await api

--- EssentialApp/EssentialAppTests/Helpers/LoaderSpy.swift
+++ EssentialApp/EssentialAppTests/Helpers/LoaderSpy.swift
@@ -58,69 +58,94 @@ class LoaderSpy {
     }
     
     //MARK: - FeedImageDataLoader
-    private struct NoResponse: Error {}
-    private struct Timeout: Error {}
     
-    private var imageRequests = [(
-        url: URL,
-        publisher: AsyncThrowingStream<Data, Error>,
-        continuation: AsyncThrowingStream<Data, Error>.Continuation,
-        result: AsyncResult?
-    )]()
+    private var imageLoader = AsyncLoaderSpy<URL, Data>()
     
-    enum AsyncResult {
-        case success
-        case failure
-        case cancelled
+    var loadedImageURLs: [URL] {
+        return imageLoader.requests.map { $0.param }
     }
     
-    private(set) var cancelledImageURLs = [URL]()
-    var loadedImageURLs: [URL] {
-        return imageRequests.map { $0.url }
+    var cancelledImageURLs: [URL] {
+        return imageLoader.requests.filter({ $0.result == .cancelled }).map { $0.param }
     }
     
     func loadImageData(from url: URL) async throws -> Data {
-        let (stream, continuation) = AsyncThrowingStream<Data, Error>.makeStream()
-        let index = imageRequests.count
-        imageRequests.append((url, stream, continuation, nil))
+        try await imageLoader.load(url)
+    }
+    
+    func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
+        imageLoader.complete(with: imageData, at: index)
+    }
+    
+    func completeImageLoadingWithError(at index: Int = 0) {
+        imageLoader.fail(with: anyNSError(), at: index)
+    }
+    
+    func result(at index: Int, timeout: TimeInterval = 1) async throws -> AsyncResult {
+        try await imageLoader.result(at: index, timeout: timeout)
+    }
+    
+    func cancelPendingRequests() async throws {
+        try await imageLoader.cancelPendingRequests()
+    }
+}
+
+enum AsyncResult {
+    case success
+    case failure
+    case cancelled
+}
+
+@MainActor
+class AsyncLoaderSpy<Param, Resource> where Resource: Sendable {
+    
+    private(set) var requests = [(
+        param: Param,
+        publisher: AsyncThrowingStream<Resource, Error>,
+        continuation: AsyncThrowingStream<Resource, Error>.Continuation,
+        result: AsyncResult?
+    )]()
+    
+    private struct NoResponse: Error {}
+    private struct Timeout: Error {}
+    
+    func load(_ param: Param) async throws -> Resource {
+        let (stream, continuation) = AsyncThrowingStream<Resource, Error>.makeStream()
+        let index = requests.count
+        requests.append((param, stream, continuation, nil))
         
         do {
             for try await result in stream {
                 try Task.checkCancellation()
-                imageRequests[index].result = .success
+                requests[index].result = .success
                 return result
             }
             try Task.checkCancellation()
             throw NoResponse()
         } catch {
-            if Task.isCancelled {
-                cancelledImageURLs.append(url)
-                imageRequests[index].result = .cancelled
-            } else {
-                imageRequests[index].result = .failure
-            }
+            requests[index].result = Task.isCancelled ? .cancelled : .failure
             throw error
         }
     }
     
-    func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
-        imageRequests[index].continuation.yield(imageData)
-        imageRequests[index].continuation.finish()
+    func complete(with resource: Resource, at index: Int = 0) {
+        requests[index].continuation.yield(resource)
+        requests[index].continuation.finish()
         
-        while imageRequests[index].result == nil { RunLoop.current.run(until: Date()) }
+        while requests[index].result == nil { RunLoop.current.run(until: Date()) }
     }
     
-    func completeImageLoadingWithError(at index: Int = 0) {
-        imageRequests[index].continuation.finish(throwing: anyNSError())
+    func fail(with error: Error, at index: Int = 0) {
+        requests[index].continuation.finish(throwing: error)
         
-        while imageRequests[index].result == nil { RunLoop.current.run(until: Date()) }
+        while requests[index].result == nil { RunLoop.current.run(until: Date()) }
     }
     
     func result(at index: Int, timeout: TimeInterval = 1) async throws -> AsyncResult {
         let maxDate = Date() + timeout
 
         while Date() <= maxDate {
-            if let result = imageRequests[index].result {
+            if let result = requests[index].result {
                 return result
             }
             //call await Task.yield() to suspend the current execution and allow other tasks to run before resuming to check for the result again.
@@ -131,9 +156,9 @@ class LoaderSpy {
     }
     
     func cancelPendingRequests() async throws {
-        for (index, request) in imageRequests.enumerated() where request.result == nil {
+        for (index, request) in requests.enumerated() where request.result == nil {
             request.continuation.finish(throwing: CancellationError())
-            while imageRequests[index].result == nil { await Task.yield() }
+            while requests[index].result == nil { await Task.yield() }
         }
     }
 }
